b394fde64d7f186a3a6aea50748580e2b1628022
Zulububub <45508795+Zulububub@users.noreply.github.com>
2020-03-21 15:26:55 +0200

    Add files via upload

---
+++ Laba6/Laba6/DataBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Laba6
+{
+    public class Names
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+    public class DataBase
+    {
+        public static List<Names> names = new List<Names>();
+        public static List<Names> Get()
+        {
+            using (StreamReader sr = new StreamReader("c:/MSI/test.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] words = line.Split(new char[] { ' ' });
+                    names.Add(new Names() { Name = words[0], Surname = words[1], PhoneNumber = words[2] });
+                }
+            }
+            return names;
+        }
+        public static void Set(string item)
+        {
+            using(StreamWriter sw = new StreamWriter("c:/MSI/test.txt", true))
+            {
+                sw.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Laba6/Laba6/MyNames.cs
@@ -0,0 +1,26 @@
+﻿using CommandDotNet.Attributes;
+using ConsoleTables;
+using System.Linq;
+
+namespace Laba6
+{
+    public class MyNames
+    {
+        [ApplicationMetadata(Description = "List of names")]
+        public void GetNames()
+        {
+            ConsoleTable
+                .From(DataBase.Get())
+                .Write();
+        }
+        [ApplicationMetadata(Description = "Add new name")]
+        public void SetName(string v1, string v2, string v3)
+        {
+            string name = System.String.Join(" ", v1, v2, v3);
+            DataBase.Set(name);
+            System.Console.WriteLine("---------------------");
+            System.Console.WriteLine("| New name is added |");
+            System.Console.WriteLine("---------------------");
+        }
+    }
+}
